bcb6d56f1ab2256a2b2655fdc1dea8dd78140ed6
JescapsAntwi <antwijescaps1@gmail.com>
2025-08-02 17:18:42 +0000

    successfully deployed pixelpipe to gcp

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -9,23 +9,6 @@ terraform {
   }
 }
 
-# Variables
-variable "project_id" {
-  description = "GCP Project ID"
-  type        = string
-}
-
-variable "region" {
-  description = "GCP Region"
-  type        = string
-  default     = "us-central1"
-}
-
-variable "environment" {  
-  description = "Environment (dev/staging/prod)"
-  type        = string
-  default     = "dev"
-}
 
 # Provider Configuration
 provider "google" {
@@ -200,28 +183,3 @@ resource "google_cloud_run_service_iam_member" "run_all_users" {
   member   = "allUsers"
 }
 
-# Outputs
-output "csv_bucket_name" {
-  description = "Name of the CSV source bucket"
-  value       = google_storage_bucket.csv_source.name
-}
-
-output "images_bucket_name" {
-  description = "Name of the processed images bucket"
-  value       = google_storage_bucket.processed_images.name  
-}
-
-output "cloud_run_url" {
-  description = "URL of the Cloud Run service"
-  value       = google_cloud_run_v2_service.image_processor.uri
-}
-
-output "pubsub_topic" {
-  description = "Pub/Sub topic for image processing"
-  value       = google_pubsub_topic.image_processing.name
-}
-
-output "service_account_email" {
-  description = "Service account email"
-  value       = google_service_account.pixelpipe_sa.email
-}
---
+++ infrastructure/monitoring.tf
@@ -0,0 +1,36 @@
+﻿# Cloud Monitoring Dashboard
+resource "google_monitoring_dashboard" "pixelpipe_dashboard" {
+  dashboard_json = jsonencode({
+    displayName = "PixelPipe Monitoring Dashboard"
+    gridLayout = {
+      columns = 2
+      widgets = [
+        {
+          title = "Cloud Function Executions"
+          xyChart = {
+            dataSets = [{
+              timeSeriesQuery = {
+                timeSeriesFilter = {
+                  filter = "metric.type=\"cloudfunctions.googleapis.com/function/execution_count\" AND resource.type=\"cloud_function\""
+                }
+                unitOverride = "1"
+              }
+            }]
+          }
+        },
+        {
+          title = "Cloud Run Request Latency"
+          xyChart = {
+            dataSets = [{
+              timeSeriesQuery = {
+                timeSeriesFilter = {
+                  filter = "metric.type=\"run.googleapis.com/request_latencies\" AND resource.type=\"cloud_run_revision\""
+                }
+              }
+            }]
+          }
+        }
+      ]
+    }
+  })
+}
